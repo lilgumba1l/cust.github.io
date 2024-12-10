495a413c2c28626c2370489bbf96dae77f015121
lilgumba1l <74672884+lilgumba1l@users.noreply.github.com>
2024-12-10 07:28:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,13 +37,28 @@ function isElementInView(element) {
   });
 // Переключение видимости левой боковой панели
 document.querySelector('.sidebar-toggle').addEventListener('click', function() {
-  document.querySelector('.sidebar').classList.toggle('open');
+  const sidebar = document.querySelector('.sidebar');
+  const roleSidebar = document.querySelector('.role-sidebar');
+
+  // Закрываем правую панель, если она открыта
+  if (roleSidebar.classList.contains('open')) {
+    roleSidebar.classList.remove('open');
+  }
+
+  // Переключаем левую панель
+  sidebar.classList.toggle('open');
 });
 
 // Переключение видимости правой боковой панели
 document.querySelector('.role-sidebar-toggle').addEventListener('click', function() {
-  document.querySelector('.role-sidebar').classList.toggle('open');
+  const sidebar = document.querySelector('.sidebar');
+  const roleSidebar = document.querySelector('.role-sidebar');
+
+  // Закрываем левую панель, если она открыта
+  if (sidebar.classList.contains('open')) {
+    sidebar.classList.remove('open');
+  }
+
+  // Переключаем правую панель
+  roleSidebar.classList.toggle('open');
 });
-
-
-  
